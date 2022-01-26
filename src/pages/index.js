295241768b6fec75245b4ba6ca96378d76113e61
Luthfi Aminulloh <luthfi.aminulloh@gmail.com>
2022-01-26 10:16:48 +0700

    add yarn

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/src/pages/Resume.md">
+            to="/Resume">
             Resume 😂
           </Link>
         </div>
